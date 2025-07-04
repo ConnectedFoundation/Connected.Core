4a19dbb5c79a3af50204c813f594eb08db9ee5c7
Tom Pipinic <tom@tompit.net>
2025-07-04 20:24:34 +0200

    feat: add default tags length constant

--- src/Connected.Core.Services.Extensions/Dto.cs
+++ src/Connected.Core.Services.Extensions/Dto.cs
@@ -5,6 +5,7 @@ public class Dto : IDto
 	public const int DefaultCodeLength = 128;
 	public const int DefaultNameLength = 128;
 	public const int DefaultTitleLength = 128;
+	public const int DefaultTagsLength = 1024;
 	public static IDto Empty => new Dto();
 	public static IDto Factory => new Dto();
 }
