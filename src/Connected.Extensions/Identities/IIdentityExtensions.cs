fb2b91419c142db57d250515f1c39da52c4d4522
mihaelocvirk <ocvirkmihael@gmail.com>
2025-11-25 15:57:35 +0100

    fix: add service url to identity extensions

--- src/Connected.Extensions/Identities/IIdentityExtensions.cs
+++ src/Connected.Extensions/Identities/IIdentityExtensions.cs
@@ -7,9 +7,9 @@ namespace Connected.Identities;
 [Service, ServiceUrl(IdentitiesUrls.Identities)]
 public interface IIdentityExtensions
 {
-	[ServiceOperation(ServiceOperationVerbs.Get)]
+	[ServiceOperation(ServiceOperationVerbs.Get), ServiceUrl("select-descriptor")]
 	Task<IIdentityDescriptor?> Select(IValueDto<string> dto);
 
-	[ServiceOperation(ServiceOperationVerbs.Get)]
+	[ServiceOperation(ServiceOperationVerbs.Get), ServiceUrl("query-descriptors")]
 	Task<IImmutableList<IIdentityDescriptor>> Query(IValueListDto<string> dto);
 }
