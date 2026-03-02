a754d09dcbb26074ac90d3f57e1ff4b2f236c619
mihaelocvirk <ocvirkmihael@gmail.com>
2026-03-02 21:52:25 +0100

    fix: return type of collectionProviderOperation

--- src/Connected.Extensions/Services/Ops/CollectionProviderOperation.cs
+++ src/Connected.Extensions/Services/Ops/CollectionProviderOperation.cs
@@ -1,7 +1,4 @@
-using Connected.Data.Expressions.Translation.Rewriters;
 using Connected.Services.Middlewares;
-using Google.Protobuf.WellKnownTypes;
-using System;
 using System.Collections.Immutable;
 
 namespace Connected.Services.Ops;
@@ -9,13 +6,13 @@ namespace Connected.Services.Ops;
 public class CollectionProviderOperation<TDto, TCollectionItem, TMiddleware>(IMiddlewareService middlewares) : ServiceFunction<TDto, IImmutableList<TCollectionItem>> where TDto : IDto
 																																												 where TMiddleware : ICollectionProviderMiddleware<TCollectionItem, TDto>
 {
-	protected override async Task<ImmutableList<TCollectionItem>> OnInvoke()
+	protected override async Task<IImmutableList<TCollectionItem>> OnInvoke()
 	{
 		var valueProviders = await middlewares.Query<TMiddleware>();
 
 		var tasks = valueProviders.Select(e => e.Invoke(Dto));
 		var results = await Task.WhenAll(tasks);
 
-		return [.. results.SelectMany(e => e)];
+		return ImmutableList.CreateRange(results.SelectMany(e => e));
 	}
 }
