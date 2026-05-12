9e3a28e8ceebcb064c2326eef494e1007acd7f33
mihaelocvirk <ocvirkmihael@gmail.com>
2026-05-12 11:12:21 +0200

    feat: register Dto

---
+++ src/Connected.Extensions/Bootstrapper.cs
@@ -0,0 +1,12 @@
+﻿using Connected.Runtime;
+using Connected.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Connected;
+internal sealed class Bootstrapper : Startup
+{
+	protected override void OnConfigureServices(IServiceCollection services)
+	{
+		services.AddTransient<IDto, Dto>();
+	}
+}
