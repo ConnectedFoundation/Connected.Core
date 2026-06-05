675f0b8fc7020ac32ffeb48fb57e469cd5b4936c
mihaelocvirk <ocvirkmihael@gmail.com>
2026-06-05 22:06:54 +0200

    fix: subscribe in events

--- src/Connected/Net/Events/Ops/Subscribe.cs
+++ src/Connected/Net/Events/Ops/Subscribe.cs
@@ -1,5 +1,4 @@
 using Connected.Net.Events.Dtos;
-using Connected.Net.Messaging;
 using Connected.Services;
 
 namespace Connected.Net.Events.Ops;
@@ -15,23 +14,14 @@ internal sealed class Subscribe(EventClients eventClients, EventSubscriptions su
 
 		var target = $"{Dto.Service}/{Dto.Event}".ToLowerInvariant();
 
-		if (subscriptions.Items.TryGetValue(target, out List<IClient>? clients))
-		{
-			if (clients is null)
-				throw new NullReferenceException("Client list is null.");
+		var clients = subscriptions.Items.GetOrAdd(target, _ => []);
 
-			lock (clients)
-			{
-				if (clients.FirstOrDefault(f => string.Equals(f.Connection, Dto.Connection, StringComparison.OrdinalIgnoreCase)) is not null)
-					return;
-
-				clients.Add(client);
-			}
-		}
-		else
+		lock (clients)
 		{
-			if (!subscriptions.Items.TryAdd(target, [client]))
-				throw new InvalidOperationException($"Cannot add subscription ({Dto.Service}/{Dto.Event})");
+			if (clients.FirstOrDefault(f => string.Equals(f.Connection, Dto.Connection, StringComparison.OrdinalIgnoreCase)) is not null)
+				return;
+
+			clients.Add(client);
 		}
 
 		await Task.CompletedTask;
