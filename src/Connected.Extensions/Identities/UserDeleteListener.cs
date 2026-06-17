8afde14ddfb8c3d68c1ab8db92012d277c9beb32
mihaelocvirk <ocvirkmihael@gmail.com>
2026-06-17 14:58:35 +0200

    feat: add delete auth token on user delete

---
+++ src/Connected.Extensions/Identities/UserDeleteListener.cs
@@ -0,0 +1,37 @@
+﻿using Connected.Annotations;
+using Connected.Entities;
+using Connected.Identities.Authentication;
+using Connected.Identities.Authentication.Dtos;
+using Connected.Notifications;
+using Connected.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connected.Identities;
+
+[Middleware<IUserService>(ServiceEvents.Deleted)]
+internal sealed  class UserDeleteListener(IIdentityAuthenticationTokenService identityAuthenticationTokenService)
+	: EventListener<IPrimaryKeyDto<int>>
+{
+	protected override async Task OnInvoke()
+	{
+		var user = Sender.GetState<IUser>().Required();
+
+		var keys = await identityAuthenticationTokenService.Query(DtoFactory.Create<IQueryIdentityAuthenticationTokensDto>(f =>
+		{
+			f.Identity = user.Token;
+		}));
+
+		foreach (var key in keys)
+		{
+			await identityAuthenticationTokenService.Delete(DtoFactory.Create<IPrimaryKeyDto<long>>(f =>
+			{
+				f.Id = key.Id;
+			}));
+		}
+	}
+}
